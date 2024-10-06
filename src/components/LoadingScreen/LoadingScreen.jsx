e167c2ee9830bdba4f2213d9a58fa5ad4fecf0c6
Abhishek <97351911+AbhishekRavi063@users.noreply.github.com>
2024-10-06 16:36:42 +0530

    3 loaders added

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './LoadingScreen.css';
-import loadingVideo from '../../Assets/loading.mp4'; // Path to your loading video
+import loadingVideo3 from '../../Assets/loading.mp4'; // Path to your first loading video
+import loadingVideo2 from '../../Assets/spaceship.mp4'; // Path to your second loading video
+import loadingVideo1 from '../../Assets/sapceship2.mp4'
 
 const LoadingScreen = ({ onLoadComplete }) => {
+  // Randomly select one of the two loading videos
+  const loadingVideos = [loadingVideo1, loadingVideo2, loadingVideo3];
+  const selectedVideo = loadingVideos[Math.floor(Math.random() * loadingVideos.length)];
+
   return (
     <div className="loading-screen">
       <video 
@@ -11,7 +17,7 @@ const LoadingScreen = ({ onLoadComplete }) => {
         className="loading-video" 
         onEnded={onLoadComplete} // Call onLoadComplete when video ends
       >
-        <source src={loadingVideo} type="video/mp4" />
+        <source src={selectedVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <p className="loading-text">Loading the moon...</p>
